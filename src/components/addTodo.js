6fc8d22dcd35d46a23e9b042675aa5862007e741
unknown <vnkn0wn5h3ll@gmail.com>
2019-06-03 06:23:55 -0400

    React

---
+++ src/components/addTodo.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+export class addTodo extends Component {
+  state = {
+    title: ""
+  };
+
+  onChange = e => this.setState({ title: e.target.value });
+
+  OnSubmit = e => {
+    e.preventDefault();
+    this.props.addTodo(this.state.title);
+    this.setState({ title: "" });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.OnSubmit} style={{ display: "flex" }}>
+        <input
+          type="text"
+          name="title"
+          style={{ flex: "10", padding: "5px" }}
+          placeholder="Add Todo ..."
+          value={this.state.title}
+          onChange={this.onChange}
+        />
+        <input
+          type="submit"
+          value="Submit"
+          className="btn"
+          style={{ flex: "1" }}
+        />
+      </form>
+    );
+  }
+}
+
+export default addTodo;
